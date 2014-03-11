8e86890459b29134a0616d62156df6f179b070f9
Bob Ippolito <bob@redivi.com>
2014-03-11 09:40:39 -0700

    single JS file

--- js/game.js
+++ js/game.js
@@ -1,44 +1,48 @@
+/*jslint sloppy:true, browser: true, devel: true, eqeq: true, vars: true, white: true*/
+/*global debugPanel:true, me:true*/
+
+// Make this false if you don't want to open the debug panel immediately
+var DEBUG_MODE = true;
 
 /* Game namespace */
 var game = {
 
 	// an object where to store game information
-	data : {
+	data: {
 		// score
 		score : 0
 	},
 	
-	
 	// Run on page load.
-	"onload" : function () {
-	// Initialize the video.
-	if (!me.video.init("screen", 480, 320, true, 'auto')) {
-		alert("Your browser does not support HTML5 canvas.");
-		return;
-	}
+	onload: function () {
+        // Initialize the video.
+        if (!me.video.init("screen", 640, 480, true, 'auto')) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
 
-	// add "#debug" to the URL to enable the debug Panel
-	if (document.location.hash === "#debug") {
-		window.onReady(function () {
-			me.plugin.register.defer(debugPanel, "debug");
-		});
-	}
+        // add "#debug" to the URL to enable the debug Panel
+        if (DEBUG_MODE || document.location.hash === "#debug") {
+            window.onReady(function () {
+                me.plugin.register.defer(debugPanel, "debug");
+            });
+        }
 
-	// Initialize the audio.
-	me.audio.init("mp3,ogg");
+        // Initialize the audio.
+        me.audio.init("mp3,ogg");
 
-	// Set a callback to run when loading is complete.
-	me.loader.onload = this.loaded.bind(this);
+        // Set a callback to run when loading is complete.
+        me.loader.onload = this.loaded.bind(this);
 
-	// Load the resources.
-	me.loader.preload(game.resources);
+        // Load the resources.
+        me.loader.preload(game.resources);
 
-	// Initialize melonJS and display a loading screen.
-	me.state.change(me.state.LOADING);
-},
+        // Initialize melonJS and display a loading screen.
+        me.state.change(me.state.LOADING);
+    },
 
 	// Run on game resources loaded.
-	"loaded" : function () {
+	loaded: function () {
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
 
@@ -46,3 +50,146 @@ var game = {
 		me.state.change(me.state.PLAY);
 	}
 };
+
+/* Game resources */
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+     */
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
+	 */
+];
+
+/* Game HUD (heads-up display) */
+game.HUD = {};
+
+game.HUD.Container = me.ObjectContainer.extend({
+
+	init: function() {
+		// call the constructor
+		this.parent();
+		
+		// persistent across level change
+		this.isPersistent = true;
+		
+		// non collidable
+		this.collidable = false;
+		
+		// make sure our object is always draw first
+		this.z = Infinity;
+
+		// give a name
+		this.name = "HUD";
+		
+		// add our child score object at the top left corner
+		this.addChild(new game.HUD.ScoreItem(5, 5));
+	}
+});
+
+
+/** 
+ * a basic HUD item to display score
+ */
+game.HUD.ScoreItem = me.Renderable.extend({	
+	/** 
+	 * constructor
+	 */
+	init: function(x, y) {
+		
+		// call the parent constructor 
+		// (size does not matter here)
+		this.parent(new me.Vector2d(x, y), 10, 10); 
+		
+		// local copy of the global score
+		this.score = -1;
+
+		// make sure we use screen coordinates
+		this.floating = true;
+	},
+
+	/**
+	 * update function
+	 */
+	update : function () {
+		// we don't do anything fancy here, so just
+		// return true if the score has been updated
+		if (this.score !== game.data.score) {	
+			this.score = game.data.score;
+			return true;
+		}
+		return false;
+	},
+
+	/**
+	 * draw the score
+	 */
+	draw : function (context) {
+		// draw it baby !
+	}
+
+});
+
+/* Play screen */
+
+game.PlayScreen = me.ScreenObject.extend({
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {
+		// reset the score
+		game.data.score = 0;
+
+		// add our HUD to the game world
+		this.HUD = new game.HUD.Container();
+		me.game.world.addChild(this.HUD);
+	},
+
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		// remove the HUD from the game world
+		me.game.world.removeChild(this.HUD);
+	}
+});
+
+/* Title screen */
+
+game.TitleScreen = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+		// TODO
+	},
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		// TODO
+	}
+});
